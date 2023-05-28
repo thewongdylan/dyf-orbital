e7d3eb52a7519fef547e97159b42a3151cee7885
thewongdylan <dylanwong.p@gmail.com>
2023-05-28 22:35:44 +0800

    Added Start Screen and Ghost

---
+++ Oslo's Odyssey/Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    [SerializeField] private float damage;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    } 
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    } 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player took damage");
+            collision.gameObject.GetComponent<OsloLife>().TakeDamage(damage);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            // enemy takes a hit, loses HP
+            Hit();
+        } 
+        else 
+        {
+            // enemy dies
+            Die();
+        }
+    }
+
+    private void Hit()
+    {
+        Debug.Log("enemy takes a hit");
+        // rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("hit"); // hit animation
+    }
+
+    private void Die()
+    {
+        Debug.Log("enemy dies");
+        // rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death"); // death animation
+        Destroy(transform.gameObject);
+    }
+}
