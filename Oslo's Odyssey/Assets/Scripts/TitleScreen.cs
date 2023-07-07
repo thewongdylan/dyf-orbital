e9c19164a63474bb2c0bb44d741d152226e94bb7
thewongdylan <dylanwong.p@gmail.com>
2023-07-07 18:41:33 +0800

    Added Title Cutscene
    Button could be improved it doesn't rly look like a button

--- Oslo's Odyssey/Assets/Scripts/TitleScreen.cs
+++ Oslo's Odyssey/Assets/Scripts/TitleScreen.cs
@@ -1,60 +1,36 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public class TitleScreen : MonoBehaviour
-// {
-//     [SerializeField] private Camera mainCamera;
-//     [SerializeField] private Image titleCard;
-//     [SerializeField] private GameObject oslo;
-//     [SerializeField] private float speed = 3f;
-//     [SerializeField] private GameObject endpoint;
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject oslo;
+    [SerializeField] private Transform osloEndpoint;
+    [SerializeField] private float osloSpeed = 10f;
+    private Animator anim;
 
-//     public void TitleScreenAnimation()
-//     {   
-//         Debug.Log("button pressed");
-//         // InvokeRepeating("MoveTitleCard", 1f, 0.1f);
-//         // InvokeRepeating("MoveCamera", 1f, 0.1f);
-//         // InvokeRepeating("MoveOslo", 0f, 0.1f);
-//         MoveOslo();
-//         // Update();
-//     }
+    public void Start()
+    {
+        anim = oslo.GetComponent<Animator>();
+    }
 
 
-//     private void MoveCamera()
-//     {
-//         Debug.Log("camera starts moving");
-//         Vector3 cameraPos = mainCamera.transform.position; 
-//         cameraPos = Vector3.MoveTowards(
-//             cameraPos, 
-//             endpoint.transform.position, 
-//             Time.deltaTime * speed
-//         );
-//         Debug.Log("camera stops moving");
-//     }
+    public void StartButtonPress()
+    {
+        StartCoroutine(OsloAnimation());
+    }
 
-//     private void MoveTitleCard()
-//     {
-//         Debug.Log("titleCard starts moving");
-//         Vector3 titleCardPos = titleCard.GetComponent<RectTransform>().position; 
-//         titleCardPos = Vector3.MoveTowards(
-//             titleCardPos, 
-//             endpoint.transform.position, 
-//             Time.deltaTime * speed
-//         );
-//         // titleCardPos.x--;
-//         Debug.Log("titleCard stops moving");
-//     }
-
-//     private void MoveOslo()
-//     {
-//         // oslo.transform.position = Vector2.MoveTowards(
-//         //     oslo.transform.position,
-//         //     endpoint.transform.position,
-//         //     Time.deltaTime * speed
-//         // );
-//         oslo.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
-//         Debug.Log("oslo should be moving");
-//     }
-// }
+    public IEnumerator OsloAnimation()
+    {
+        while (oslo.transform.position != osloEndpoint.position)
+        {
+            oslo.transform.position = Vector3.MoveTowards(
+                oslo.transform.position, 
+                osloEndpoint.position, 
+                osloSpeed * Time.deltaTime);
+            yield return 0;
+            anim.SetInteger("state", 1); // running animation
+        }
+    }
+}
