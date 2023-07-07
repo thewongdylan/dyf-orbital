8fd08fa4a98836e57e29ed6e6a7a15f2938b6194
thewongdylan <dylanwong.p@gmail.com>
2023-07-08 01:37:22 +0800

    Redid TutorialSelect with buttons
    Got rid of oslo

--- Oslo's Odyssey/Assets/Scripts/Tiles/JumpSceneTile.cs
+++ Oslo's Odyssey/Assets/Scripts/Tiles/JumpSceneTile.cs
@@ -6,15 +6,16 @@ using UnityEngine.SceneManagement;
 public class JumpSceneTile : MonoBehaviour
 {
     [SerializeField] private int sceneIndex;
+    [SerializeField] private float loadDelay = 0f;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player"))
         {
-            JumpToScene();
+            Invoke("JumpToScene", loadDelay);
         }
     }
 
-    private void JumpToScene()
+    public void JumpToScene()
     {
         SceneManager.LoadScene(sceneIndex);
     }
--- Oslo's Odyssey/Assets/Scripts/Tiles/NextSceneTile.cs
+++ Oslo's Odyssey/Assets/Scripts/Tiles/NextSceneTile.cs
@@ -6,16 +6,18 @@ using UnityEngine.SceneManagement;
 public class NextSceneTile : MonoBehaviour
 {
     // private bool levelCompleted = false;
+    [SerializeField] private float loadDelay = 0f;
+
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) //&& levelCompleted == false)
         {
             // levelCompleted = true; // Needed when playing sound effects for level complete etc
-            LoadNextScene();
+            Invoke("LoadNextScene", loadDelay);
         }
     }
 
-    private void LoadNextScene()
+    public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
